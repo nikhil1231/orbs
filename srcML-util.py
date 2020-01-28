d1e855b8e63c3533a4c69296cadb0c2f764b284e
Nithin-Anand <nithin@hotmail.co.uk>
2020-01-28 01:50:00 +0000

    Added  srcML conversion

--- srcML-util.py
+++ srcML-util.py
@@ -1,11 +1,43 @@
 # utility file with srcML helper functions
+import os
+import shutil
 
 
 def dir_to_xml(path):
     # convert project directory to srcML directory
-    pass
+    working_directory = path + '_sliced'
+    shutil.copytree(path, working_directory)
+    for (root,dirs,files) in os.walk(working_directory, topdown=True):
+        for source_file in files:
+            file_extension = determine_extension(source_file)
+            if (file_extension == 'invalid' or file_extension == 'xml'):
+                continue
+            else:
+                file_path = root + '/' + source_file
+                xml_source = file_path.strip(file_extension) + '.xml'
+                os.system('./srcml ' + file_path + ' -o ' + xml_source)
+                os.system('rm ' + file_path)
+    
+    
 
 
 def xml_to_dir(path):
-    # convert srcML directory back to original language
-    pass
+    working_directory = path + '_sliced'
+    for (root,dirs,files) in os.walk(working_directory, topdown=True):
+        for source_file in files:
+            file_extension = determine_extension(source_file)
+            if (file_extension == 'xml'):
+                file_path = root + '/' + source_file
+                print(file_path)
+                os.system('./srcml --to-dir . ' + file_path)
+                os.system('rm ' + file_path)
+
+
+def determine_extension(src):
+    valid_extensions = ['xml','c', 'cpp', 'cc', 'java', 'c#']
+    extension = src.split('.')[-1]
+    if valid_extensions.count(extension) == 1:
+        return extension
+    else:
+        return 'invalid'
+
