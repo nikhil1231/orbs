08155f509d8cacb3a6ca5f69ec930a8e48d37a84
Nithin-Anand <nithin@hotmail.co.uk>
2020-01-31 16:20:37 +0000

    Updated the helper functions

--- srcML_util.py
+++ srcML_util.py
@@ -17,15 +17,12 @@ def clone_and_convert_target(directory_path, target_file_path):
     
 
 # converts the srcML code in the cloned directory back to slice code
-def xml_to_dir(path): #call this function with the original path, not the '[original]_sliced' path
-    working_directory = path + '_sliced'
-    for (root,dirs,files) in os.walk(working_directory, topdown=True):
-        for source_file in files:
-            if (determine_extension(source_file) == 'xml'):
-                file_path = root + '/' + source_file
-                print(file_path)
-                os.system('./srcml --to-dir . ' + file_path)
-                os.system('rm ' + file_path)
+def convert_to_source(directory_path, target_file_path): #call this function with the original path, not the '[original]_sliced' path
+    working_directory = directory_path + '_sliced'
+    target_file_path = os.path.join(working_directory, target_file_path)
+    if (determine_extension(target_file_path) == 'xml'):
+        os.system('./srcml --to-dir . ' + target_file_path)
+        os.system('rm ' + target_file_path)
 
 def determine_extension(src):
     valid_extensions = ['xml','c', 'cpp', 'cc', 'java', 'cs'] #extensions supported by srcML
