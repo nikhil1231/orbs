454cd4b5ed197d347ccbf98069427a06473ba6a9
yacoubb <yacoub.ahmedy@gmail.com>
2020-02-03 23:46:39 +0000

    srcML-util whitespace alignment

--- srcML_util.py
+++ srcML_util.py
@@ -4,6 +4,8 @@ import shutil
 import xml.etree.ElementTree as ET
 
 # clone project directory and convert target file
+
+
 def clone_and_convert_target(directory_path, target_file_path):
     working_directory = directory_path + '_sliced'
     if (os.path.isdir(working_directory)):
@@ -14,24 +16,27 @@ def clone_and_convert_target(directory_path, target_file_path):
     if (file_extension != 'invalid' and file_extension != 'xml'):
         xml_source = target_file_path.strip(file_extension) + 'xml'
         os.system('./srcml ' + target_file_path + ' -o ' + xml_source)
-        os.system('rm ' + target_file_path)
+        # os.system('rm ' + target_file_path)
     return working_directory
 
 # converts the srcML code in the cloned directory back to slice code
-def convert_to_source(directory_path, target_file_path): #call this function with the '[original]_sliced' path
+
+
+def convert_to_source(directory_path, target_file_path):  # call this function with the '[original]_sliced' path
     target_file_path = os.path.join(directory_path, target_file_path)
     if (check_xml(target_file_path)):
         os.system('./srcml --to-dir . ' + target_file_path)
-        os.system('rm ' + target_file_path)
+        # os.system('rm ' + target_file_path)
 
 
 def determine_extension(src):
-    valid_extensions = ['xml','c', 'cpp', 'cc', 'java', 'cs'] #extensions supported by srcML
+    valid_extensions = ['xml', 'c', 'cpp', 'cc', 'java', 'cs']  # extensions supported by srcML
     extension = src.split('.')[-1]
     if valid_extensions.count(extension) == 1:
         return extension
     else:
         return 'invalid'
+
 
 def check_xml(src):
     extension = src.split('.')[-1]
