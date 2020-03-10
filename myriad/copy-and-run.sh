933dccd7002de12966e5792d79243853e4d1cfe1
yacoubb <yacoub.ahmedy@gmail.com>
2020-03-10 17:19:59 +0000

    Setting python version

--- myriad/copy-and-run.sh
+++ myriad/copy-and-run.sh
@@ -1,6 +1,10 @@
 cp -R /home/zcabyih/Scratch/orbs $TMPDIR/orbs
 chmod 700 $TMPDIR/orbs
 
+module load python3/recommended
+rm -R /home/zcabyih/Scratch/workspace
+mkdir /home/zcabyih/Scratch/workspace
+
 python3 $TMPDIR/orbs/slice.py -o if >> /home/zcabyih/Scratch/out.txt
 # stdout should go to .o file in output directory
 
