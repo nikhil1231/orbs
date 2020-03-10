f4cf08c1c46f3ce22ebfb5f5fc4eebb47100fc59
yacoubb <yacoub.ahmedy@gmail.com>
2020-03-10 15:16:36 +0000

    Changing copy and run to use fixed args

--- myriad/copy-and-run.sh
+++ myriad/copy-and-run.sh
@@ -1,7 +1,7 @@
 cp -R /home/zcabyih/orbs $TMPDIR/orbs
 chmod 700 $TMPDIR/orbs
 
-python3 $TMPDIR/orbs/myriad/myriad-slice.py "$variable1" "$variable2" "$variable3" "$variable4" "$variable5" "$variable6" >> /home/zcabyih/Scratch/out.txt
+python3 $TMPDIR/orbs/slice.py -o if >> /home/zcabyih/Scratch/out.txt
 # stdout should go to .o file in output directory
 
 rm -R $TMPDIR/orbs
